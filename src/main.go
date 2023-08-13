c82f5b5ad031e02c453a3493ec4370e2c9671f81
CJ <83363242+orbitalmartian8@users.noreply.github.com>
2023-08-13 21:14:14 +0100

    Update main.go

--- src/main.go
+++ src/main.go
@@ -18,6 +18,20 @@ func main() {
         tmpl.Execute(w, nil)
     })
     
+    router.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+        instance := r.FormValue("instance")
+        userId := r.FormValue("userId")
+        
+        // Save instance and userId to session or database
+        
+        http.Redirect(w, r, "/timeline", http.StatusSeeOther)
+    })
+    
+    router.HandleFunc("/timeline", func(w http.ResponseWriter, r *http.Request) {
+        tmpl := template.Must(template.ParseFiles("templates/timeline.html"))
+        tmpl.Execute(w, nil)
+    })
+    
     // Initialize server
     http.Handle("/", router)
     http.ListenAndServe(":8080", nil)
