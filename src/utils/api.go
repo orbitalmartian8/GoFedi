062b53faed29092a0a8ea199b97d6b8a3727779a
CJ <83363242+orbitalmartian8@users.noreply.github.com>
2023-08-13 21:05:17 +0100

    Create api.go

---
+++ src/utils/api.go
@@ -0,0 +1,10 @@
+// utils/api.go
+package utils
+
+import (
+    "net/http"
+)
+
+func FetchUserTimeline(accessToken string) ([]byte, error) {
+    // Implement logic to fetch user timeline using Mastodon API
+}
